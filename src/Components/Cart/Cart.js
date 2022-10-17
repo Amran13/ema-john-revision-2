4faf6c8c01723c2c7bfa3bd170650f234119c911
amranhossain <amranhossainchy@gmail.com>
2022-10-17 15:24:07 +0600

    react set up done

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Cart.css';
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const {cart} = props;
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    let tax  = parseFloat((total * 0.1).toFixed(2));
     return (
-        <div>
-            <h1>Order Summery</h1>
-            <h2> Orderred: {cart.length} </h2>
+        <div className='cart'>
+            <h2>Order Summery</h2>
+            <p> Orderred: {cart.length} </p>
+            <p>Total Price :  ${total} </p>
+            <p>Total Shipping :  ${shipping}</p>
+            <p>Tax : ${tax} </p>
+            <h3>Grand Total : ${total + shipping + tax} </h3>
+
         </div>
     );
 };
